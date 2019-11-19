c04a6c126400361dd9cf1e3c5b0c68d1fadd1c31
Fatkhur R <fatkhur139wec@gmail.com>
2019-11-19 15:20:56 +0700

    get next deep paging

--- index.js
+++ index.js
@@ -28,10 +28,12 @@ const file_json = 'result.json';
   //deeper loop
   for (var abj_syn of synonimData) {
     await page.goto(abj_syn.link)
-    const AbjWord = await page.evaluate(() => {
+    const nav_els = await page.$$('.article-post .text-center span')
+    // console.log(nav_els[1])
+    const AbjWord = await page.evaluate( () => {
       let link_abjword = []
       let AbjWordEl = document.querySelectorAll('.article-post ul li')
-      AbjWordEl.forEach(async abj_wordel => {
+      AbjWordEl.forEach( abj_wordel => {
         let abjword_json = {}
         try {
           abjword_json.attribute = abj_wordel.querySelector('a').text
@@ -42,36 +44,68 @@ const file_json = 'result.json';
         }
         link_abjword.push(abjword_json)
         
-      // get page number
-    let nav_page = document.querySelectorAll('.article-post .text-center span')
-    if(nav_page.length > 0){
-      let last_arrs = nav_page[nav_page.length - 1] 
-      let last_page = last_arrs.querySelector('span a').text
-      let current_url = window.location.href 
-      let regs = /[0-9]+(?!.*[0-9])/;
-      for (let index = 2; index <= last_page; index++) {
-        var next_url = current_url.replace(regs,index)
-        await page.goto(next_url)
-        //doing something 
-        const next_page =  await page.evaluate(() => {
-          let el_next_page = document.querySelectorAll('.article-post ul li')
-          el_next_page.forEach(element => {
-            let next_page_json = {}
-            try {
-              next_page_json.attribute = element.querySelector('a').text
-              next_page_json.link = element.querySelector('a').getAttribute('href')
-            } catch (error) {
-              console.error(error)
-            }
-            link_abjword.push(next_page_json)
-          });
-        })
-      }
-    }
+        // get page number
+        // let nav_page = document.querySelectorAll('.article-post .text-center span')
+        // if(nav_page.length > 0){
+        //   let last_arrs = nav_page[nav_page.length - 1] 
+        //   let last_page = last_arrs.querySelector('span a').text
+        //   let current_url = window.location.href 
+        //   let regs = /[0-9]+(?!.*[0-9])/;
+        //   for (let index = 2; index <= last_page; index++) {
+        //     var next_url = current_url.replace(regs,index)
+        //     await page.goto(next_url)
+        //     //doing something 
+        //     const next_page =  await page.evaluate(() => {
+        //       let el_next_page = document.querySelectorAll('.article-post ul li')
+        //       el_next_page.forEach(element => {
+        //         let next_page_json = {}
+        //         try {
+        //           next_page_json.attribute = element.querySelector('a').text
+        //           next_page_json.link = element.querySelector('a').getAttribute('href')
+        //         } catch (error) {
+        //           console.error(error)
+        //         }
+        //         link_abjword.push(next_page_json)
+        //       });
+        //     })
+        //     console.dir(next_page)
+        //   }
+        // }
       });
       return link_abjword
     })
     
+    if( nav_els.length > 0){
+      // var max_nav = nav_els[ nav_els.length - 1 ]
+     const temps = await page.evaluate( () => {
+       let all_temps = document.querySelectorAll('.article-post .text-center span')
+       let max_all = all_temps[ all_temps.length -1 ]
+       const all_vals = max_all.querySelector('span a').text
+      return all_vals
+     })
+
+     let regs = /[0-9]+(?!.*[0-9])/;
+     var current_url = page.url();
+     for (let index = 2; index <= temps; index++) {
+      var next_url = current_url.replace(regs,index)
+      await page.goto(next_url)
+      const next_pages = await page.evaluate( () => {
+        let el_next_page = document.querySelectorAll('.article-post ul li')
+        el_next_page.forEach(element => {
+          let next_page_json = {}
+          try {
+            next_page_json.attribute = element.querySelector('a').text
+            next_page_json.link = element.querySelector('a').getAttribute('href')
+          } catch (error) {
+            console.error(error)
+          }
+          arr_temp.push(next_page_json)
+        });
+      })
+       
+     }
+
+    }
     arr_temp.push(AbjWord)
     // console.dir(arr_temp)
   }
